55d9292987676464d1cc3aee50ef9b3b960ed113
NITSAN Technologies <sanjay@nitsan.in>
2019-04-05 20:56:58 +0530

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,10 +20,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.2',
+    'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.0.0-9.5.5',
+            'typo3' => '6.0.0-9.9.9',
         ],
         'conflicts' => [],
         'suggests' => [],
