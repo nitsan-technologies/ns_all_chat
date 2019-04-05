edef19f44f2f7b35456757d5cd811289ad3d8201
Sanjay Chauhan <sanjay@nitsan.in>
2019-04-05 16:04:31 +0530

    Minor changes in author and title

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,10 +10,10 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => '[NITSAN] All in One Chat – Real Time Chat',
+    'title' => '[NITSAN] All in One Chat',
     'description' => 'One of the only TYPO3 extension which provides to use most popular chat tool at your website. This TYPO3 extension provides to configure many live chat tools eg., zopim.com, livechatinc.com, purechat.com, livezilla.net, clickdesk.com, tidiochat.com, visitlead.com, onwebchat.com, userlike.com, hipchat.com & more will be available in an upcoming version.',
     'category' => 'plugin',
-    'author' => 'Sanjay Chuahan [NITSAN Technologies]',
+    'author' => 'NITSAN Technologies',
     'author_email' => 'sanjay@nitsan.in',
     'state' => 'stable',
     'internal' => '',
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.0.0-9.3.3',
+            'typo3' => '6.0.0-9.5.5',
         ],
         'conflicts' => [],
         'suggests' => [],
