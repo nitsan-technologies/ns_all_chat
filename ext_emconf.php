4a9d59761df66af5b10521cf456a8e70518f8abe
NITSAN Technologies <sanjay@nitsan.in>
2019-05-25 12:04:26 +0530

    Adding Contributors and Minor Bug Fixing

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,29 +1,21 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext: "ns_all_chat"
- *
- *
- * Manual updates:
- * Only the data in the array - anything else is removed by next write.
- * "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] All in One Chat',
     'description' => 'One of the only TYPO3 extension which provides to use most popular chat tool at your website. This TYPO3 extension provides to configure many live chat tools eg., zopim.com, livechatinc.com, purechat.com, livezilla.net, clickdesk.com, tidiochat.com, visitlead.com, onwebchat.com, userlike.com, hipchat.com & more will be available in an upcoming version.',
     'category' => 'plugin',
-    'author' => 'NITSAN Technologies',
-    'author_email' => 'sanjay@nitsan.in',
+    'author' => 'T3:Bhavin Barad, T3:Keval Pandya, QA:Siddharth Sheth',
+	'author_email' => 'sanjay@nitsan.in',
+	'author_company' => 'NITSAN Technologies Pvt Ltd',
     'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.1.0',
+    'version' => '1.1.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.0.0-9.9.9',
+            'typo3' => '6.0.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
