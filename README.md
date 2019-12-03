b248509c995ceded789bc99c4ebb12b882048c1d
NITSAN <sanjay@nitsan.in>
2019-12-03 10:55:51 +0530

    [TODO] Adding Gitlab-CI & Travis with PHP, TypoScript, YAML lints [TODO] Updating documentation [TODO] Added demo link [TODO] Releasing pro-version of the extenion at https://t3terminal.com/

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ns_all_chat
-The TYPO3 Extension: https://extensions.typo3.org/extension/ns_all_chat/
+
+- FREE version: You can download FREE version with basic-features at https://extensions.typo3.org/extension/ns_all_chat/
+- PRO version: You can download PRO version with more-features & free-support at https://t3terminal.com/typo3-all-in-one-chat-plugin-free/
--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] All in One Chat',
-    'description' => 'One of the only TYPO3 extension which provides to use most popular chat tool at your website. This TYPO3 extension provides to configure many live chat tools eg., zopim.com, livechatinc.com, purechat.com, livezilla.net, clickdesk.com, tidiochat.com, visitlead.com, onwebchat.com, userlike.com, hipchat.com & more will be available in an upcoming version.',
+    'description' => 'One of the only TYPO3 extension which provides to use most popular chat tool at your website. This TYPO3 extension provides to configure many live chat tools eg., zopim.com, livechatinc.com, purechat.com, livezilla.net, clickdesk.com, tidiochat.com, visitlead.com, onwebchat.com, userlike.com, hipchat.com etc. Live-Demo: https://demo.t3terminal.com/t3t-extensions/ You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-all-in-one-chat-plugin-free/',
     'category' => 'plugin',
     'author' => 'T3:Bhavin Barad, T3:Keval Pandya, QA:Siddharth Sheth',
 	'author_email' => 'sanjay@nitsan.in',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.1.1',
+    'version' => '1.2.0',
     'constraints' => [
         'depends' => [
             'typo3' => '6.0.0-9.5.99',
