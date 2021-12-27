026d5f8c623f2cf0e8958bda01d757db3cb33a7a
Maulik Lakhnotra <nitsan-gitlab@fischer-and-friends.de>
2021-12-27 11:08:09 +0530

    [TODO] Compatiblity to version 11

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,14 +4,19 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 $_EXTKEY = 'ns_all_chat';
 
+if (version_compare(TYPO3_branch, '10.0', '>')) {
+    $moduleClass = \Nitsan\NsAllChat\Controller\NsAllChatsController::class;
+} else {
+    $moduleClass = 'NsAllChats';
+}
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Nitsan.NsAllChat',
     'Frnsallchat',
     [
-        'NsAllChats' => 'script'
+        $moduleClass => 'script'
     ],
     // non-cacheable actions
     [
-        'NsAllChats' => ''
+        $moduleClass => ''
     ]
 );
