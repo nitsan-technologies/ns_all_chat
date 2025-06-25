cec1aaf87e78b5fe4704cc029bedbc02a08468d5
NITSAN <sanjay@nitsan.in>
2025-06-25 12:47:52 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,13 @@
 <?php
 
 $EM_CONF['ns_all_chat'] = [
-    'title' => 'All in One Chat',
-    'description' => 'The All in One TYPO3 Chat Extension is the only TYPO3 extension that helps to integrate the most popular chat tools directly into your website. With this extension, you can easily connect various live chat tools such as zopim.com, livechatinc.com, purechat.com, and more. 
+    'title' => 'TYPO3 All-in-One Chat Integration',
+    'description' => 'Easily integrate multiple popular live chat tools like Zopim, LiveChatInc, and PureChat into your TYPO3 site to improve user communication and support.',
     
-    *** Live-Demo: https://demo.t3planet.com/t3-extensions/all-chat *** Documentation & Free Support: https://t3planet.com/typo3-chat-extension',
     'category' => 'plugin',
-    'author' => 'T3: Nilesh Malankiya, T3: Himanshu Ramavat, QA: Krishna Dhapa',
-    'author_email' => 'sanjay@nitsan.in',
-    'author_company' => 'T3Planet // NITSAN',
+    'author' => 'Team T3Planet',
+    'author_email' => 'info@t3planet.de',
+    'author_company' => 'T3Planet',
     'state' => 'stable',
     'version' => '13.0.0',
     'constraints' => [
